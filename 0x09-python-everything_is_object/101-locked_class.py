135ede9e11decc1b2716cbc0cb3a379ed4bbff7a
Ashdof <mzigah4@gmail.com>
2023-01-04 16:28:30 +0000

    Task 101 - fixed requirement issue

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,4 +3,12 @@
 
 
 class LockedClass:
+    """A locked class
+
+    Description:
+        this is a class that prevents the user from dynamically
+        creating new instance attributes
+
+    """
+
     __slots__ = ['first_name']
