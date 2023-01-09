6f725df9422f6fc8da056ba9930131eb54ac72a3
Ashdof <mzigah4@gmail.com>
2023-01-09 15:28:33 +0000

    Created a function that returns a list of attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """Lookup attributes
+
+    Description:
+        This function looks up and returns a list of attributes
+        and methods of an obejct defined by its parameter
+
+    Args:
+        obj (object): the object parameter
+
+    Returns:
+        A list of attributes and methods of the object
+    """
+
+    return dir(obj)
