73a1bbe423a4bee462a42fa686c089bb86906f32
Ashdof <mzigah4@gmail.com>
2023-01-09 18:24:38 +0000

    Task 6 - Added a function the base class that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A Class to model a geometry"""
+
+
+class BaseGeometry:
+    """Model a geometry object"""
+
+    def area(self):
+        """Raise an exception"""
+        raise Exception("area() is not implemented")
