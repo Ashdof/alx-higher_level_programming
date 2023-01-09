048c926b39b39cdb0acba769642198e9551d9a0f
Ashdof <mzigah4@gmail.com>
2023-01-09 18:05:31 +0000

    Task 4 - Created a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Determine the instance of an object"""
+
+
+def inherits_from(obj, a_class):
+    """Instance of a class
+
+    Description:
+        This function determines whether an onject is an instance of
+        a class that inherited (directly or indirectly) from the
+        specified class
+
+    Args:
+        obj (object): the object parameter
+        a_class (class): the class parameter
+
+    Returns:
+        True if obj is an instance of a class that inherited
+        (directly or indirectly) from the specified class or
+        False otherwise
+    """
+    return [True if issubclass(type(obj), a_class) and
+            obj != a_class else False]
