4db6f01965e16d6243eda1f8aa426cefa55a123a
Ashdof <mzigah4@gmail.com>
2023-01-09 17:52:37 +0000

    Task 3 - Created a function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Determine whether an object is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Instance of a class
+
+    Description:
+        This function determines whether an onject is an instance of
+        a class or an instance of a class that inherist from the
+        specified class
+
+    Args:
+        obj (object of a class): the object parameter
+        a_class (class): the class parameter
+
+    Returns:
+        True if obj is an instance of a_class or False otherwise
+
+    """
+
+    if type(obj) == a_class or isinstance(obj, a_class):
+        return True
+
+    return False
