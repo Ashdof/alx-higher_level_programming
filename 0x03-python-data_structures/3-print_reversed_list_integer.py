7409f2042a290ea20ec41b2996fc48c4ef57147b
Ashdof <mzigah4@gmail.com>
2024-01-06 14:07:31 +0000

    Task 3 - a function that prints elements of a list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """
+    Print all elements in reverse
+
+    my_list: a list of elements
+    """
+    if isinstance(my_list, list):
+        my_list.reverse()
+
+        for i in my_list:
+            print("{}".format(i))
