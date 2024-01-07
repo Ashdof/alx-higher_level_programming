0a11c8ed43f1f8d1b99abc4d0f3498690de388ee
Ashdof <mzigah4@gmail.com>
2024-01-07 12:39:39 +0000

    Task 4 - a program that prints all the names defined in a compiled file

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """A program that prints all names defined by hidden_4 module"""
+
+    import hidden_4
+
+    vals = dir(hidden_4)
+    for val in vals:
+        if val[:2] != '__':
+            print(val)
