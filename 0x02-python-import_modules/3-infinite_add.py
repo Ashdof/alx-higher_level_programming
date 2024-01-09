c574785e52348d114a519fddcbb20d0509f93b7d
Ashdof <mzigah4@gmail.com>
2024-01-09 10:15:12 +0000

    Task 3 - improved code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,9 +5,7 @@ if __name__ == "__main__":
     import sys
 
     ans = 0
-    args = sys.argv
-    n = len(args) - 1
 
-    for i in range(n):
-        ans += int(args[i + 1])
+    for i in range(len(sys.argv) - 1):
+        ans += int(sys.argv[i + 1])
     print("{:d}".format(ans))
