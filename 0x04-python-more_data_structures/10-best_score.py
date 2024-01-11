4921914c574d2547b1313ea06199bab0fe0aa863
Ashdof <mzigah4@gmail.com>
2024-01-11 10:57:41 +0000

    Task 10 - fix code success issue

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -9,7 +9,7 @@ def best_score(a_dictionary):
 
     Return: key with biggest values
     """
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
 
     return max(a_dictionary, key=a_dictionary.get)
