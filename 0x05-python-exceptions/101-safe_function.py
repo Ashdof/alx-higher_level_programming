faa7195dd960046686c52df71f5e54c91a861a9e
Ashdof <mzigah4@gmail.com>
2024-01-24 12:40:04 +0000

    Task 8 - a function that executes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        rs = fct(*args)
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+        rs = None
+
+    return rs
