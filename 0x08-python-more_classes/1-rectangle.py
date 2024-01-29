801ab50d9be5dd642eb01be22fafb19d9994bcd7
Ashdof <mzigah4@gmail.com>
2024-01-29 14:45:48 +0000

    Task 1 - added width and height attributes to class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""A module with a class"""
+
+
+class Rectangle:
+    """An class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize the rectangle object
+
+        Args:
+            width (int): the width value
+            height (int): the height value
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Return the width of the rectangle"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Update the width of the rectangle
+
+        Args:
+            value (int): the value to update the width
+        """
+
+        err_type = "width must be an integer"
+        err_value = "width must be >= 0"
+
+        if not isinstance(value, int):
+            raise TypeError(err_type)
+        elif value < 0:
+            raise ValueError(err_value)
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Return the height of this rectangle"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Update the height of the rectangel
+
+        Args:
+            value (int): the value to update the rectangle
+        """
+
+        err_type = "height must be an integer"
+        err_value = "height must be >= 0"
+
+        if not isinstance(value, int):
+            raise TypeError(err_type)
+        elif value < 0:
+            raise ValueError(err_value)
+        else:
+            self.__height = value
