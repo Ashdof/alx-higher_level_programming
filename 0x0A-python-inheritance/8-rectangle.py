77f9c98141bd6a762bc469a69df7f2840ecaf798
Ashdof <mzigah4@gmail.com>
2024-02-05 13:25:54 +0000

    Task 8 - improve initialization function

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(BaseGeometry):
             height (int): The height of the new Rectangle
         """
 
-        self.integer_validator("width", width)
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
