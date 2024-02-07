ebd69ad209cce5ea948ddccf2a897d0ecc8dd55b
Ashdof <mzigah4@gmail.com>
2024-02-07 09:27:19 +0000

    Task 9 - a class that defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A module with a Student class"""
+
+import json
+
+
+class Student:
+    """Model a Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize object
+
+        Args:
+            first_name (str): the first name of the student
+            last_name (str): the last name of the student
+            age (int): the age of the student
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieve a dictionary representation of Student object"""
+
+        return self.__dict__
