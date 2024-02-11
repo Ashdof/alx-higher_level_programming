3a537faf8d92e4f01dce28260a3ad5eef0a2a313
Ashdof <mzigah4@gmail.com>
2024-02-11 09:19:13 +0000

    Task 15 - added a method to base to return its json string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """A module with a base class"""
 
+import json
+
 
 class Base:
     """The base class"""
@@ -19,3 +21,24 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        JSON string
+
+        Description:
+            This method creates a JSON string representation
+            of a list of dictionaries
+
+        Args:
+            list_dictionaries (lis of dictionaries): a list of
+            dictionary data
+
+        Returns:
+            JSON string representation of the data
+        """
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries, indent=4)
