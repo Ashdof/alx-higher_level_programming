88f6c1e67711489742590ec57c6b9db8a6c20b05
Ashdof <mzigah4@gmail.com>
2024-02-15 12:48:52 +0000

    Task 6 - added a method to return a string representation of the rectangle instance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -208,3 +208,9 @@ class Rectangle(Base):
             for j in range(self.width):
                 print('#', end='')
             print("")
+
+    def __str__(self):
+        """Return a string representation of this rectangle object"""
+
+        info = "[Rectangle] ({}) {}/{} - {}/{}"
+        return info.format(self.id, self.x, self.y, self.width, self.height)
