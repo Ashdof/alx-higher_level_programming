232b27e474dc7b2d91dd33cbe93596b6f03cb4cb
Ashdof <mzigah4@gmail.com>
2024-04-10 08:40:11 +0000

    Task 6 - a script that prints an array using a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Print an array using a loop
+const info = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < info.length) {
+  console.log(info[i]);
+  i++;
+}
