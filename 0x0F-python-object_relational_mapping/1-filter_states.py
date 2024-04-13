8e138eb711f908eed74ea0f6db008ba7d8c15b34
Ashdof <mzigah4@gmail.com>
2024-04-13 11:55:50 +0000

    Task 1 - a script that selects all states with name starting with N from the database

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" List all states from the database hbtn_0e_0_usa with the name
+starting with the letter N
+
+Usuage:
+./0-select_states.py <mysql username> <mysql password> <database name>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = con.cursor()
+    sql = "SELECT * FROM `states` ORDER BY `id`"
+    cur.execute(sql)
+
+    for state in cur.fetchall():
+        if state[1][0] == "N":
+            print(state)
